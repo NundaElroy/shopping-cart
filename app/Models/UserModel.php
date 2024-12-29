4f00bbc25dc99c56215eddf9576e5c4967eeef42
NundaElroy <sekyezaelroy@gmail.com>
2024-12-29 16:08:34 +0300

    (edit) set timestamps to false

---
+++ app/Models/UserModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table            = 'users';
+    protected $primaryKey       = 'user_id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['email','password'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+  
+
+    // Dates
+    protected $useTimestamps = false;
+          // If soft deletes are not used
+    
+   
+   
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+   
+}
